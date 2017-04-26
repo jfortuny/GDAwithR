c9eb98b00bad2cff814f9072fad62df648e1a6db
jfortuny <jfortuny@jfortuny.com>
2017-04-26 07:25:44 -0400

    more on parallel coordinate plots

--- code/Chapter06.R
+++ code/Chapter06.R
@@ -11,5 +11,24 @@ ggparcoord(data = food, columns = c(1:7), scale="uniminmax") +
 
 # Figure 6.2
 food1 <- food/food$Wt
-ggparcoord(data = food1, columns=c(1:5, 7), 
-           scale="uniminmax", alphaLines=0.2) + xlab("") + ylab("")
+head(food1)
+ggparcoord(data = food1, 
+           columns=c(1:5, 7), 
+           scale="uniminmax", 
+           alphaLines=0.2) + xlab("") + ylab("")
+
+# Figure 6.3
+food1 <- within(food1,
+                fatX <- factor(ifelse(Fat > 0.75, 1, 0)))
+ggparcoord(data = food1[order(food1$fatX),],
+           columns=c(1:5, 7), 
+           groupColumn="fatX",
+           scale="uniminmax") + xlab("") + ylab("")  +
+  theme(legend.position = "none") + 
+  coord_flip()
+
+# Figure 6.4
+ggplot(food1, aes(Protein, Carbohyd)) + geom_point()
+
+# Figure 6.5
+ggparcoord(iris, columns=1:4, groupColumn="Species")
