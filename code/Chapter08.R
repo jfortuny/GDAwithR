e0a2a52b48f31f62c9f67c5d24fa29814c31149a
jfortuny <jfortuny@jfortuny.com>
2017-05-05 14:19:20 -0400

    worked on chapter 8

--- code/Chapter08.R
+++ code/Chapter08.R
@@ -24,3 +24,32 @@ par(mfrow = n2mfrow(length(vcs)))
 relativeWeight <- with(HI, wght/sum(as.numeric(wght))*100)
 for(v in vcs) 
   barplot(tapply(relativeWeight, HI[[v]], sum), main = v)
+
+# Figure 8.3
+data(Boston, package="MASS")
+par(mfrow=c(1,2))
+for (i in c("chas", "rad")) {
+  barplot(table(Boston[, i]),
+          main=(paste("Barchart of", i)))
+}
+
+# Figure 8.4
+vs1 <- !(names(Boston) %in% c("chas","rad"))
+grs <- n2mfrow(sum(as.numeric(vs1)))
+par(mfrow=grs)
+for (i in names(Boston)[vs1]) {
+  hist(Boston[,i], col="grey70", xlab="", ylab="",
+       main=(paste("Histogram of", i)))
+}
+
+# Figure 8.5
+plot(Boston, pch=16)
+
+# Figure 8.6
+data(Boston, package="MASS")
+par(mfrow=c(1,1), mar=c(3.1, 1.1, 2.1, 1.1))
+MASS::parcoord(Boston)
+
+# Figure 8.7
+library(gplots)
+heatmap.2(as.matrix(Boston), scale="column", trace="none")
