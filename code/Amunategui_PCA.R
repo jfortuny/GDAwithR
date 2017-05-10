7f7fcab8775a056c0bc65d65d15f2237c677c140
jfortuny <jfortuny@jfortuny.com>
2017-05-10 12:28:06 -0400

    completed pca analysis

--- code/Amunategui_PCA.R
+++ code/Amunategui_PCA.R
@@ -276,5 +276,69 @@ predictions <- predict(object = gbm_model,
                        type = "raw")
 head(predictions)
 postResample(pred = predictions, obs = testdf$cluster)
+# Accuracy     Kappa 
+# 0.9586667 0.9172863 
+
 varImp(gbm_model, scale = FALSE)
-plot(varImp(gbm_model, scale = FALSE), top=50)
+plot(varImp(gbm_model, scale = FALSE), top=20)
+
+# we can save the varImp results in a data frame so we can use them
+vimp <- varImp(gbm_model, scale = FALSE)
+results <- data.frame(row.names(vimp$importance), vimp$importance$Overall)
+head(results)
+results$VariableName <- row.names(vimp)
+colnames(results) <- c("VariableName", "Weight")
+results <- results[order(results$Weight),]
+head(results)
+str(results)
+results$VariableName <- as.character(results$VariableName)
+
+# let's display the best 20 features
+results_temp <- tail(results,20)
+par(mar=c(5,5,4,2))
+xx <- barplot(results_temp$Weight, 
+              width = 0.85,
+              main = paste("Variable Importance -",'cluster'), 
+              horiz = T,
+              xlab = "< (-) importance > < neutral > < importance (+) >", 
+              axes = FALSE,
+              col = ifelse((results_temp$Weight > 0), 'blue', 'red'))
+axis(2, 
+     at=xx, 
+     labels=results_temp$VariableName, 
+     tick=FALSE, 
+     las=2, 
+     line=-0.3, 
+     cex.axis=0.6)   
+
+# We can now try a model with the best features and see how many 
+# we need to achieve similar accuracy (although now we'll test accuracy
+# against the validation dataset)
+tail(results, 100)
+# Let's use the results with a Weight > 2
+traindf_truncated <- traindf[, c(results$VariableName[results$Weight > 2], 'cluster')]
+dim(traindf_truncated)
+# Fit the model
+fitControl <- trainControl(method="none")
+gbm_model <- train(cluster~., 
+                   data=traindf_truncated,
+                   tuneGrid = expand.grid(n.trees = 150, 
+                                          interaction.depth = 3, 
+                                          shrinkage = 0.1, 
+                                          n.minobsinnode=10),
+                   trControl=fitControl, 
+                   method="gbm", 
+                   metric='roc'
+                   )
+# now predict
+predictions <- predict(object=gbm_model, 
+                       gisette_df_validate[,setdiff(names(traindf_truncated), 'cluster')], 
+                       type='raw')
+# Verify accuracy
+head(predictions)
+head(gisette_df_validate$cluster)
+# We need to convert gisette_df_validate$cluster to "yes" and "no"
+postResample(pred=predictions, obs=as.factor(ifelse(gisette_df_validate$cluster == 1,"yes", "no")))
+# Not too bad
+# Accuracy     Kappa 
+# 0.9536667 0.9072691 
