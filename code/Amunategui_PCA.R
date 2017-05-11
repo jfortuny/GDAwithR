813f02c9c2c5cdd5480e8bb3bb911922ea76b9c7
jfortuny <jfortuny@jfortuny.com>
2017-05-11 14:09:45 -0400

    completed amunategui

--- code/Amunategui_PCA.R
+++ code/Amunategui_PCA.R
@@ -494,3 +494,98 @@ feats <- mRMR.ensemble(data = mRMR_data,
 bestVars <-data.frame('features'=names(traindf)[solutions(feats)[[1]]], 
                       'scores'= scores(feats)[[1]])
 bestVars
+
+
+# docs 07 ------------------------------------------------------------------
+library(fscaret)
+
+# get the data
+titanicDF <- read.csv('http://math.ucdenver.edu/RTutorial/titanic.txt',sep='\t')
+
+titanicDF$Title <- ifelse(grepl('Mr', titanicDF$Name),'Mr',
+                   ifelse(grepl('Mrs',titanicDF$Name),'Mrs',
+                   ifelse(grepl('Miss',titanicDF$Name),'Miss','Nothing')))
+titanicDF$Age[is.na(titanicDF$Age)] <- median(titanicDF$Age, na.rm=T)
+
+# miso format
+titanicDF <- titanicDF[c('PClass', 'Age', 'Sex', 'Title', 'Survived')]
+# prepare the data
+titanicDF$Title <- as.factor(titanicDF$Title)
+titanicDummy <- dummyVars("~.",data=titanicDF, fullRank=F)
+titanicDF <- as.data.frame(predict(titanicDummy,titanicDF))
+print(names(titanicDF))
+# to enable probabilities we need to force outcome to factor
+titanicDF$Survived <- as.factor(ifelse(titanicDF$Survived==1, 'yes','no'))
+
+# split the data
+set.seed(1234)
+splitIndex <- sample(nrow(titanicDF), floor(0.8*nrow(titanicDF)))
+traindf <- titanicDF[ splitIndex,]
+testdf <- titanicDF[-splitIndex,]
+dim(traindf)
+dim(testdf)
+
+# evaluate the data
+myFS.class <-fscaret(traindf, 
+                     testdf, 
+                     myTimeLimit = 20,
+                     preprocessData = TRUE, 
+                     with.labels = TRUE,
+                     classPred = TRUE,
+                     regPred = FALSE,
+                     Used.funcClassPred = c("gbm","rpart","pls"),
+                     supress.output = FALSE, 
+                     no.cores = NULL,
+                     saveModel = FALSE)
+# results - Variable importance
+results <- myFS.class$VarImp$matrixVarImp.MeasureError
+# beautify the results
+results$Input_no <- as.numeric(results$Input_no)
+results <- results[,setdiff(names(results), c('SUM%','ImpGrad'))]
+myFS.class$PPlabels$Input_no <- as.numeric(rownames(myFS.class$PPlabels))
+results <- merge(x=results, 
+                 y=myFS.class$PPlabels, 
+                 by="Input_no",
+                 all.x=T)
+results <- results[order(-results$SUM),]
+results
+
+# Run the top 5 variables through PLS and then measure the ROC/AUC
+traindf_truncated <- traindf[, c(head(as.character(results$Labels),5),
+                                 'Survived')]
+dim(traindf_truncated)
+# prepare the run
+objControl <- trainControl(method='cv', 
+                           number=3, 
+                           returnResamp='none',
+                           summaryFunction = twoClassSummary, 
+                           classProbs = TRUE)
+# pls model
+set.seed(1234)
+pls_model <- train(Survived~., data=traindf_truncated, 
+                   method="pls",
+                   metric='ROC', 
+                   trControl=objControl)
+pls_predictions <- predict(object=pls_model,
+                           testdf[,setdiff(names(traindf_truncated), 'Survived')], 
+                           type='prob')
+library(pROC)
+print(auc(predictor=pls_predictions[[2]],
+          response=ifelse(testdf$Survived=='yes',1,0)))
+
+# we could also loop through models for final comparison of accuracy
+method_names <- c("C5.0", "gbm", "rf")
+for (method_name in method_names) {
+  print(method_name)
+  set.seed(1234)
+  model <- train(Survived~., 
+                 data=traindf_truncated,
+                 method=funcClassPred[funcClassPred==method_name],
+                 metric='ROC',
+                 trControl=objControl)
+  predictions <- predict(object=model,
+                         testdf[,setdiff(names(traindf_truncated), 'Survived')], 
+                         type='prob')
+  print(auc(predictor=predictions[[2]],
+            response=ifelse(testdf$Survived=='yes',1,0)))
+}
